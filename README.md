58de16b6a1e442db4d255ebb05bfa1dec6f294b5
ccbury <conordebru@gmail.com>
2024-01-02 18:01:04 +0000

    Added axios call for getting data from backend

---
+++ README.md
@@ -0,0 +1,7 @@
+# React + TypeScript + Vite
+
+Use the command `npm start`, within the client-app directory to run the frontend
+
+# .NET API
+
+Use thee command `dotnet watch --no-hot-reload`, within the API firectory to ron the backend
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
+import { Header, List } from 'semantic-ui-react';
+
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
+
   return (
     <>
-     <h1>Quote Bible</h1>
+    <Header as='h2' icons='users' content='Quote Bible'/>
+     <List>{activities.map((activity: any) => (
+      <List.Item key={activity.id}>
+        {activity.title}
+      </List.Item>
+     ))}</List>
     </>
   )
 }
