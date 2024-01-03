a4ef22cc54a883c4c2caf7321a3e42ddb2141aaf
ccbury <conordebru@gmail.com>
2024-01-03 03:18:43 +0000

    Added Activity Interface and NavBar. Updated folder structure

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Use the command `npm start`, within the client-app directory to run the frontend
 # .NET API
 
 Use thee command `dotnet watch --no-hot-reload`, within the API firectory to ron the backend
+
+
+Use `dotnet restore` to update dotnet projects when imports are not working
---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './styles.css'
+
+import { List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
+
+  return (
+    <>
+      <NavBar />
+      <List>{activities.map(activity => (
+        <List.Item key={activity.id}>
+          {activity.title}
+        </List.Item>
+      ))}</List>
+    </>
+  )
+}
+
+export default App
