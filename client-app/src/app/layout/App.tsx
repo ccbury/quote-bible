08db5915de1ea37be25ff4af8043d090d127412e
ccbury <conordebru@gmail.com>
2024-01-04 02:56:36 +0000

    Added loading indicator and added time delay for temp testing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
-import axios from 'axios';
 import { v4 as uuid } from 'uuid';
 
 import './styles.css'
@@ -8,16 +7,25 @@ import './styles.css'
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import agent from '../api/agent';
+import LoadingComponent from './loadingComponent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities')
+    agent.Activities.list()
       .then(response => {
-        setActivities(response.data);
+        const activities: Activity[] = [];
+        response.forEach(activity => {
+          activity.date = activity.date.split('T')[0];
+          activities.push(activity)
+        })
+        setActivities(activities);
+        setLoading(false);
       })
   }, [])
 
@@ -48,6 +56,7 @@ function App() {
     setActivities([...activities.filter(x => x.id !== id)])
   }
 
+  if (loading) return <LoadingComponent content='Loading app' />
   return (
     <>
       <NavBar openForm={handleFormOpen} />
